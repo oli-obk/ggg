ec5ccb49efc308f7d7084df4e70885889ed7ee5f
Oliver Schneider <mail@oli-obk.de>
2013-10-14 21:16:01 +0200

    hello cairo

--- main.cpp
+++ main.cpp
@@ -12,6 +12,9 @@
 #include <sstream> // For int <-> string conversion
 #include <vector>
 
+#include <cairomm/cairomm.h>
+#include "optional.hpp"
+
 enum class ZOrder
 {
     Background,
@@ -23,13 +26,40 @@ enum class ZOrder
 class GameWindow : public Gosu::Window
 {
     Gosu::Font font;
-
+    optional<Gosu::Image> img;
 public:
     GameWindow()
     :Window(640, 480, false)
     ,font(graphics(), Gosu::defaultFontName(), 20)
     {
         setCaption(L"GraphGame");
+        auto surface = Cairo::ImageSurface::create(
+                        Cairo::FORMAT_ARGB32,
+                        240,
+                        80
+                        );
+        auto ctx = Cairo::Context::create(surface);
+        
+        ctx->select_font_face("serif", Cairo::FONT_SLANT_NORMAL, Cairo::FONT_WEIGHT_BOLD);
+        ctx->set_font_size(32.0);
+        ctx->set_source_rgb(0.0, 0.0, 1.0);
+        ctx->move_to(10.0, 50.0);
+        ctx->show_text("Hello, world");
+        
+        surface->flush();
+        auto data = surface->get_data();
+        data = surface->get_data();
+        Gosu::Bitmap bmp(surface->get_width(), surface->get_height());
+        Gosu::Color* it = bmp.data();
+        for (size_t i = 0; i < surface->get_width()*surface->get_height(); i++) {
+            it->setAlpha(data[3]);
+            it->setRed(data[2]);
+            it->setGreen(data[1]);
+            it->setBlue(data[0]);
+            it++;
+            data += 4;
+        }
+        img.emplace(std::ref(graphics()), bmp);
     }
 
     void update()
@@ -38,6 +68,10 @@ public:
 
     void draw()
     {
+        if(img) {
+            img->draw(0, 0, 0);
+            font.draw(L"bla", 0, 0, 1);
+        }
     }
 
     void buttonDown(Gosu::Button btn)
---
+++ optional.hpp
@@ -0,0 +1,261 @@
+/*
+ * taken from http://stackoverflow.com/questions/11875045/implementing-boostoptional-in-c11
+ */
+#ifndef OPTIONAL_HPP
+#define OPTIONAL_HPP
+#include <exception>
+#include <array>
+#include <type_traits>
+#include <cassert>
+
+template<typename T, class Enable = void>
+class optional;
+
+template<typename T>
+optional<T> make_optional(const T& val)
+{
+	return optional<T>(val);
+}
+
+template<typename T>
+optional<T> make_optional(T&& val)
+{
+	return optional<T>(std::move(val));
+}
+
+template<typename T>
+class optional<T, typename std::enable_if<!std::is_reference<T>::value>::type>
+{
+	friend class optional<T&>;
+	friend class optional<const T&>;
+private:
+    bool valid;
+    // needs to be a union, b/c in a union nothing is initialized by default
+    union {
+        T value;
+    };
+    
+    void destruct_if_valid()
+    {
+        if (valid) {
+            value.~T();
+        }
+    }
+    
+public:
+    // default constructor
+    optional() : valid(false) {}
+    
+    // initializer
+    explicit optional(const T& _v)
+    {
+        valid = true;
+        new (&value) T(_v);
+    }
+
+	explicit optional(T&& _v)
+    {
+        valid = true;
+        new (&value) T(std::move(_v));
+    }
+    
+    // copy constructor
+    optional(const optional& other)
+    {
+        valid = other.valid;
+        if (valid) {
+            new (&value) T(other.value);
+        }
+    }
+    
+    optional& operator=(const optional& other)
+    {
+        destruct_if_valid();
+        valid = other.valid;
+        new (&value) T(other.value);
+        return *this;
+    }
+    
+    optional& operator=(optional&& other)
+    {
+        destruct_if_valid();
+        valid = other.valid;
+        if (valid) {
+            new (&value) T(std::move(other.value));
+            other.valid = false;
+        }
+        return *this;
+    }
+    
+    // move constructor
+    optional(optional&& other)
+    {
+        valid = other.valid;
+        if (valid) {
+            new (&value) T(std::move(other.value));
+            other.valid = false;
+        }
+    }
+
+    ~optional()
+    {
+        destruct_if_valid();
+    }
+
+    bool operator!() const { return !valid; }
+    
+    operator bool() const { return valid; }
+
+    T& operator*()
+    {
+        assert(valid);
+        if (valid) return value;
+        throw std::bad_exception();
+    }
+    
+    const T& operator*() const
+    {
+        assert(valid);
+        if (valid) return value;
+        throw std::bad_exception();
+    }
+    
+    T* operator->()
+    {
+        assert(valid);
+        if (valid) return &value;
+        throw std::bad_exception();
+    }
+    
+    const T* operator->() const
+    {
+        assert(valid);
+        if (valid) return &value;
+        throw std::bad_exception();
+    }
+    
+    template<typename... Args>
+    void emplace(Args... args)
+    {
+        destruct_if_valid();
+        valid = true;
+        new (&value) T(args...);
+    }
+    
+    void clear()
+    {
+        destruct_if_valid();
+        valid = false;
+    }
+};
+
+template<typename T3>
+class optional<T3, typename std::enable_if<std::is_reference<T3>::value>::type >
+{
+private:
+    typedef typename std::remove_reference<T3>::type T;
+    T* value;
+
+public:
+    // default constructor
+    optional() : value(nullptr) {}
+    
+    // initializer
+    explicit optional(const T3 _v) : value(&_v) {}
+    
+    // copy constructor
+    optional(const optional& other)
+    {
+        value = other.value;
+    }
+
+	template<typename T5>
+	explicit optional(const optional<T5>& other)
+	{
+		if (!other) {
+			value = nullptr;
+		} else {
+			value = const_cast<T*>(&other.value);
+		}
+	}
+    
+    friend class optional<const T&>;
+    
+    template<typename T4 = T>
+    optional(const optional<typename std::remove_const<T4>::type&> other, typename std::enable_if<std::is_const<T4>::value>::type* = nullptr)
+    {
+        value = other.value;
+    }
+    
+    optional& operator=(const optional& other)
+    {
+        value = other.value;
+        return *this;
+    }
+	
+	optional& operator=(optional&& other)
+    {
+        value = other.value;
+		other.value = nullptr;
+        return *this;
+    }
+    
+    // move constructor
+    optional(optional&& other)
+	#if __GNUC__ > 4 || (__GNUC__ == 4 && __GNUC_MINOR__ >= 7)
+	:optional()
+    {
+    #else
+    {
+        value = nullptr;
+	#endif
+        std::swap(value, other.value);
+    }
+
+    ~optional()
+    {
+    }
+
+    bool operator!() const { return !value; }
+    
+    operator bool() const { return value; }
+
+    T& operator*()
+    {
+        assert(value);
+        if (value) return *value;
+        throw std::bad_exception();
+    }
+    
+    const T& operator*() const
+    {
+        assert(value);
+        if (value) return *value;
+        throw std::bad_exception();
+    }
+    
+    T* operator->()
+    {
+        assert(value);
+        if (value) return value;
+        throw std::bad_exception();
+    }
+    
+    const T* operator->() const
+    {
+        assert(value);
+        if (value) return value;
+        throw std::bad_exception();
+    }
+    
+    void emplace(T3 t)
+    {
+        value = &t;
+    }
+    
+    void clear()
+    {
+        value = nullptr;
+    }
+};
+#endif // OPTIONAL_HPP
