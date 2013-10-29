d35ac42795f68eba793075e624e4cff40f0625db
Oliver Schneider <mail@oli-obk.de>
2013-10-29 19:59:29 +0100

    implemented basic graph rendering

--- main.cpp
+++ main.cpp
@@ -15,18 +15,144 @@
 #include <cairomm/cairomm.h>
 #include "optional.hpp"
 
-enum class ZOrder
+enum ZOrder
 {
-    Background,
-    Stars,
-    Player,
-    UI
+    zBackground,
+    zGraph,
+    zUI
+};
+
+class Node;
+
+class Edge
+{
+private:
+    Node* _node;
+    Edge* _other;
+public:
+    double weight;
+    Edge(Node& node)
+    :weight(0)
+    ,_node(&node)
+    {}
+    
+    Edge(Node& node, Edge& edge)
+    :Edge(node)
+    {
+        _other = &edge;
+        edge._other = this;
+    }
+    Node& node() { return *_node; }
+    Edge& other() { return *_other; }
+};
+
+namespace std {
+    template<typename T, typename... Args>
+    std::unique_ptr<T> make_unique(Args&&... args)
+    {
+        return std::unique_ptr<T>(new T(std::forward<Args>(args)...));
+    }
+}
+
+struct Node
+{
+private:
+    std::vector<std::unique_ptr<Edge>> _edges;
+public:
+    double x, y;
+    Node(double x, double y)
+    :x(x)
+    ,y(y)
+    {
+    }
+    
+    const std::vector<std::unique_ptr<Edge>>& Edges() const { return _edges; }
+    
+    optional<Edge&> connection(Node& other) const
+    {
+        for (auto& edge:_edges) {
+            if (&edge->node() == &other) {
+                return optional<Edge&>(*edge);
+            }
+        }
+        return optional<Edge&>();
+    }
+    
+    bool connect(Node& other)
+    {
+        if (connection(other)) return false;
+        auto edge = std::make_unique<Edge>(*this);
+        auto edge2 = std::make_unique<Edge>(*this, *edge);
+        _edges.push_back(std::move(edge));
+        other._edges.push_back(std::move(edge2));
+        return true;
+    }
+    
+};
+
+class Graph
+{
+    static const size_t max_nodes = 100;
+    std::array<optional<Node>, max_nodes> Nodes;
+    size_t NodeCount;
+public:
+    Graph()
+    :NodeCount(0)
+    {
+    }
+
+    Node& CreateNode(double x, double y)
+    {
+        for (auto& node:Nodes) {
+            if (node) continue;
+            node.emplace(x, y);
+            NodeCount++;
+            return *node;
+        }
+        throw std::runtime_error("too many nodes");
+    }
+
+    void DeleteNode(Node& node)
+    {
+        for (auto& n:Nodes) {
+            if (&(*n) == &node) {
+                n.clear();
+                NodeCount--;
+                return;
+            }
+        }
+        throw std::runtime_error("unknown node");
+    }
+    
+    void draw(Gosu::Graphics& g)
+    {
+        for (auto& node:Nodes) {
+            if (!node) continue;
+            double wdt = 10;
+            double hgt = 10;
+            g.drawQuad(
+                node->x - wdt, node->y - wdt, Gosu::Color::RED,
+                node->x + wdt, node->y - wdt, Gosu::Color::RED,
+                node->x + wdt, node->y + wdt, Gosu::Color::RED,
+                node->x - wdt, node->y + wdt, Gosu::Color::RED,
+                zUI
+            );
+            for (auto& edge: node->Edges()) {
+                g.drawLine(
+                    node->x, node->y, Gosu::Color::BLUE,
+                    edge->node().x, edge->node().y, Gosu::Color::BLUE,
+                    zUI
+                );
+            }
+        }
+    }
 };
 
 class GameWindow : public Gosu::Window
 {
     Gosu::Font font;
     optional<Gosu::Image> img;
+    Graph graph;
 public:
     GameWindow()
     :Window(640, 480, false)
@@ -60,6 +186,10 @@ public:
             data += 4;
         }
         img.emplace(std::ref(graphics()), bmp);
+        
+        auto& node = graph.CreateNode(100, 100);
+        auto& node2 = graph.CreateNode(150, 200);
+        node.connect(node2);
     }
 
     void update()
@@ -68,10 +198,13 @@ public:
 
     void draw()
     {
-        if(img) {
-            img->draw(0, 0, 0);
-            font.draw(L"bla", 0, 0, 1);
-        }
+        graph.draw(graphics());
+        graphics().drawTriangle(
+            input().mouseX(), input().mouseY(), Gosu::Color::WHITE,
+            input().mouseX()+20, input().mouseY()+5, Gosu::Color::WHITE,
+            input().mouseX()+5, input().mouseY()+20, Gosu::Color::WHITE,
+            zUI
+        );
     }
 
     void buttonDown(Gosu::Button btn)
